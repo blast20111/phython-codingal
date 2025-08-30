2a04023a3cc201875c2ad306c45937ee5df7afd6
blast20111 <blast.20111@gmail.com>
2025-08-30 19:57:26 +0530

    python1

---
+++ lesson1/print statements and comments.py
@@ -0,0 +1,8 @@
+#saying myself to world
+print("hello world")
+#saying my name
+print("i am abhinav")
+#telling them what i love to play
+print("i love playing soccer")
+#telling the addition of 3 and 5
+print("the sum of 3+5 is",3+5)
