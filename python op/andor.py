b220bb536f351c2ef035c24698e5e0fcde08650c
blast20111 <blast.20111@gmail.com>
2025-09-14 19:59:59 +0530

    fat

---
+++ python op/andor.py
@@ -0,0 +1,20 @@
+a=10
+b=12
+c=0
+if a and b and c:
+    print("all the values have the same boolean number")
+else:
+    print("at least one of them has boolean value")
+a=10
+b=10
+c=0
+if a>0 or b>0:
+    print("either one of the values are greater than 0")
+else:
+    print("none of them is greater than zero")
+if b>0 or c>0:
+    print("either one of them is greater than zero")
+else:
+    print("none of them is greater than zero")
+    
+     
---
+++ python op/not equal.py
@@ -0,0 +1,20 @@
+a=10
+b=12
+c=12
+print(a!=b)
+print(b!=c)
+a="Python"
+b="coding"
+if (a==b):
+    print("both are same")
+if a!=b:
+    print("both are different")
+a=4
+b=5
+if(a==1)!=(b==5):
+    print("hello")
+a=int(input("enter your number"))
+if a%2==0:
+    print("this is a even number10" )
+else:
+    print("its a odd number")
