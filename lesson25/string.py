bdc38cd9febad4bdcc0c9cd92d76a6b385f1e715
blast20111 <blast.20111@gmail.com>
2025-11-23 19:49:53 +0530

    hey

---
+++ lesson25/string.py
@@ -0,0 +1,10 @@
+class uppercase:
+    def __init__(self):
+        self.str1=""
+    def get_string(self):
+        self.str1=input("enter a string:")
+    def print_string(self):
+        print("the result is",self.str1.upper())
+str1=uppercase()
+str1.get_string()
+str1.print_string()
