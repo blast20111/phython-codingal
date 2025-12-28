753933db42f06c2c5e8621a69dc84e155d4e8dec
blast20111 <blast.20111@gmail.com>
2025-12-28 19:56:24 +0530

    porshe

--- lesson34/space.py
+++ lesson34/space.py
@@ -6,7 +6,7 @@ SCREEN_HEIGHT=500
 PLAYER_START_X=370
 PLAYER_START_Y=380
 ENEMY_START_Y_MIN=50
-ENENY_START_Y_MAX=150
+ENEMY_START_Y_MAX=150
 ENEMY_SPEED_X=4
 ENEMY_SPEED_Y=40
 BULLET_SPEED_Y=10
@@ -28,8 +28,59 @@ enemyX_change=[]
 enemyY_change=[]
 num_of_enemies=6
 for _i in range(num_of_enemies):
-    enemyImg.append(pygame.image.load)
-    enemyX.append(ENEMY_SPEED_X)
-    enemyY.append(ENEMY_SPEED_Y)
-    
+    enemyImg.append(pygame.image.load('C:\\Users\\Ascender_20111\\Desktop\\phython-codingal\\lesson34\\enemy.png'))
+
+    enemyX.append(random.randint(0, SCREEN_WIDTH - 64))
+
+    enemyY.append(random.randint(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX))
+
+    enemyX_change.append(ENEMY_SPEED_X)
+
+    enemyY_change.append(ENEMY_SPEED_Y)
+bulletImg=pygame.image.load('C:\\Users\\Ascender_20111\\Desktop\\phython-codingal\\lesson34\\bullet.png')
+bulletX=0
+bulletY=BULLET_SPEED_Y
+bulletX_change=0
+bulletY_change=BULLET_SPEED_Y
+bullet_state="ready"
+score_value=0
+font=pygame.font.Font('freesansbold.ttf',32)
+textX=10
+textY=10
+over_font=pygame.font.Font('freesansbold.ttf',64)
+def show_score(x,y):
+    score=font.render("score:"+str(score_value),True,(255,255,255))
+    screen.blit(score,(x,y))
+def game_over_text():
+    over_text=over_font.render("GAME OVER",True,(255,255,255))
+    screen.blit(over_text,(200,250))
+def player(x,y):
+    screen.blit(playerImg,(x,y))
+def enemy(x,y,i):
+    screen.blit(ememyImg[i],(x,y))
+def fire_bullet(x,y):
+    global bullet_stare
+    bullet_stare="fire"
+    screen.blit(bulletImg(x+16,y+10))
+def isCollision(enemyX,enemyY,bulletX,bulletY):
+    distance=math.sqrt((enemyX-bulletX)**2+(enemyY-bulletY)**2)
+    return distance<COLLISION_DISTANCE
+running=True
+while running:
+    screen.fill((0,0,0))
+    screen.blit(background,(0,0))
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_LEFT:
+                playerX_change=-5
+            if event.key==pygame.K_RIGHT:
+                playerX_change=5
+            if event.key==pygame.K_SPACE and bullet_state=="ready":
+                bulletX=playerX
+                fire_bullet(bulletX,bulletY)
+            
+
+
 
